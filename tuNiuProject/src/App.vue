19e8c21afd0b0e5d5e05bcab65dcab767c0bc0a3
Dave Chou <Chouzyb@163.com>
2019-06-25 15:27:57 +0800

    fixed some envirment bug

---
+++ tuNiuProject/src/App.vue
@@ -0,0 +1,13 @@
+<style>
+
+</style>
+
+<template>
+    <div class="app">
+        <h1>Hello Would</h1>
+    </div>
+</template>
+
+
+
+
--- tuNiuProject/webpack.config.js
+++ tuNiuProject/webpack.config.js
@@ -6,6 +6,7 @@ module.exports = {
     entry:'./src/main.js',
     output:{
         path:path.resolve(__dirname,'.dist'),
+        publicPath:'dist',
         filename:'bundle.js',
     },
     module:{
