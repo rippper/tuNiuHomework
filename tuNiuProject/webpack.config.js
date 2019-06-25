5e4df3fef18f4a29c8df9aa72f1621056e5e15db
Dave Chou <Chouzyb@163.com>
2019-06-25 15:08:00 +0800

    create tuniu project

---
+++ tuNiuProject/webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+
+module.exports = {
+    mode:'development',
+    entry:'./src/main.js',
+    output:{
+        path:path.resolve(__dirname,'.dist'),
+        filename:'bundle.js',
+    },
+    module:{
+        rules:[
+            {
+                test:/\.css$/,
+                use:["vue-style-loader","css-loader"]
+            },
+            {
+                test:/\.vue$/,
+                loader:"vue-loader"
+            },
+            {
+                test:/\.styl$/,
+                use:["style-loader","css-loader","stylus=loader"]
+            },
+            {
+                test:/\.(jpg|png|svg)$/,
+                use:["file-loader"]
+            }
+        ]
+    },
+    plugins: [
+        // make sure to include the plugin!
+        new VueLoaderPlugin()
+    ],
+    resolve: {
+        alias: {
+          'vue$': 'vue/dist/vue.esm.js' // 用 webpack 1 时需用 'vue/dist/vue.common.js'
+        }
+    }
+}
